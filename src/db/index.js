275e13f57d68f1ebfc0b4fcb20e2edcfad70e39e
sahdev markam <sahdevmarkam60@gmail.com>
2025-08-29 21:19:48 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-import asyncHandler from "../utils/asyncHandler.async.js";
-
-
+import {Video} from '../models/video.model.js'
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     console.log(`\n mongodb connected !! DB HOST ${connectionInstance.connection.host}`);
+    await Video()
   } catch (error) {
     console.log('mongodb connection ERROR db/index.js', error);
     process.exit(1)
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema}  from "mongoose";
-import jwt from "jsonwebtocken"
+import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 const userSchema = new SchemaTypes({
@@ -54,5 +54,27 @@ userSchema.pre("seve",async function (next) {
   this.password = bcrypt.hash(this.password, 10)
   next()
 })
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({ 
+    id: this._id,
+    email: this.email, 
+    username: this.username,
+    fullname: this.fullname }, 
+    process.env.ACCESS_TOKEN_SECRET, 
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+return jwt.sign({ 
+    id: this._id
+  },
+    process.env.REFRESH_TOKEN_SECRET, 
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+  )
+}
 
 export const User = mongoose.model("User", userSchema)
