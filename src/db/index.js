79998386badec2c5f1d7459a19fb42866b76cf2e
sahdev markam <sahdevmarkam60@gmail.com>
2025-08-30 12:38:47 +0530

    test route

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
-import {Video} from '../models/video.model.js'
+import { DB_NAME } from "../constants.js"
+
+
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     console.log(`\n mongodb connected !! DB HOST ${connectionInstance.connection.host}`);
-    await Video()
   } catch (error) {
     console.log('mongodb connection ERROR db/index.js', error);
     process.exit(1)
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { resolveObjectURL } from "buffer";
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_URL
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+    // file has been uploaded successfull
+    console.log("file is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    return null;
+  }
+}
+
+
+export {uploadOnCloudinary}
